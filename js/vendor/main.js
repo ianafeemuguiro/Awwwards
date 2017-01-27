dbb7c633ff43286fb322a09d39f2384bbab82a5a
Iana Muguiro <Iana Muguiro>
2017-01-27 16:18:52 -0600

    Ya estoy terminando el archivo HTML y CSS, tuve unos problemas con javascript pero ya todo es funcional

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -5,3 +5,36 @@ $(".off-canvas-submenu-call").click(function() {
 	$(this).parent().next(".off-canvas-submenu").slideToggle('fast');
 	$(this).find("span").text(icon);
 });
+
+$(document).foundation();
+    $(".off-canvas-submenu").hide();
+    $(".off-canvas-submenu-call").click(function() {
+      var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
+      $(this).parent().next(".off-canvas-submenu").slideToggle('fast');
+      $(this).find("span").text(icon);
+    });
+
+    $(document).ready(function(){
+
+  // hide #back-top first
+  $("#back-top").hide();
+
+  // fade in #back-top
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+
+    // scroll body to 0px on click
+    $('#back-top .fi-arrow-up').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
+});
